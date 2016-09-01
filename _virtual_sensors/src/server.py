ed7fd2f73333c78a593e1a86dc0f7159a0cca24c
Antoine Auger <antoine.auger@isae.fr>
2016-09-01 17:23:07 +0200

    Virtual sensor: -APIs for enable / sensing / get and set capabilities -One main thread per sensor

--- _virtual_sensors/src/server.py
+++ _virtual_sensors/src/server.py
@@ -1,36 +1,147 @@
-from bottle import run, Bottle
+import json
 
+from bottle import run, request, response, Bottle
 from virtual_sensor import VirtualSensor
+
+# Bottle parameters
 
 app = Bottle()
 bottle_host = '127.0.0.1'
 bottle_port = 8080
 
+# Sensor identification
+
 sensorID = 's01'
+sensorEndpoint = 'http://' + bottle_host + ':' + str(bottle_port) + '/' + sensorID
 sensor = None
 
+# REST APIs for the virtual sensor (see also the corresponding class virtual_sensor.py)
+
 @app.route('/' + sensorID + '/init', method='GET')
-def initSensor():
-    return "Sensor {} initialized successfully!\n".format(sensorID)
+def init_virtual_sensor():
+    """ Dummy method for testing purpose """
+    response.set_header('Content-Type', 'application/json; charset=UTF-8')
+    dict_response = dict()
+    dict_response['status'] = "OK"
+    dict_response['error_message'] = ""
+    dict_response['additional_details'] = "Sensor {} initialized successfully!".format(sensorID)
+    return json.dumps(dict_response)
 
-@app.route('/' + sensorID, method='GET')
-def getSensorDetails():
+@app.route('/' + sensorID + '/enabled', method='GET')
+def get_sensor_enabled():
+    response.set_header('Content-Type', 'application/json; charset=UTF-8')
+    dict_response = dict()
+    dict_response['status'] = "OK"
+    dict_response['error_message'] = ""
+    dict_response['parameter'] = "enabled"
+    dict_response['value'] = sensor.enabled
+    return json.dumps(dict_response)
+
+@app.route('/' + sensorID + '/enabled', method='POST')
+def set_sensor_enabled():
+    request_json_body = json.loads(request.body.read().decode('UTF-8'))
+    old_value = sensor.enabled
+    new_value = request_json_body['new_value']
+    response.set_header('Content-Type', 'application/json; charset=UTF-8')
+    dict_response = dict()
+    if type(new_value) == bool:
+        sensor.enable_sensor(new_value)
+        dict_response['status'] = "OK"
+        dict_response['error_message'] = ""
+        dict_response['parameter'] = 'enabled'
+        dict_response['old_value'] = old_value
+        dict_response['new_value'] = new_value
+    else:
+        dict_response['status'] = "NOK"
+        dict_response['error_message'] = "Only one boolean is accepted for this POST request. E.g.: {'new_value': true}"
+    return json.dumps(dict_response)
+
+@app.route('/' + sensorID + '/sensing', method='GET')
+def get_sensor_enabled():
+    response.set_header('Content-Type', 'application/json; charset=UTF-8')
+    dict_response = dict()
+    dict_response['status'] = "OK"
+    dict_response['error_message'] = ""
+    dict_response['parameter'] = "sensing"
+    dict_response['value'] = sensor.sensing
+    return json.dumps(dict_response)
+
+@app.route('/' + sensorID + '/sensing', method='POST')
+def set_sensor_enabled():
+    request_json_body = json.loads(request.body.read().decode('UTF-8'))
+    old_value = sensor.enabled
+    new_value = request_json_body['new_value']
+    response.set_header('Content-Type', 'application/json; charset=UTF-8')
+    dict_response = dict()
+    if type(new_value) == bool:
+        sensor.enable_sensing_process(new_value)
+        dict_response['status'] = "OK"
+        dict_response['error_message'] = ""
+        dict_response['parameter'] = 'sensing'
+        dict_response['old_value'] = old_value
+        dict_response['new_value'] = new_value
+    else:
+        dict_response['status'] = "NOK"
+        dict_response['error_message'] = "Only one boolean is accepted for this POST request. E.g.: {'new_value': true}"
+    return json.dumps(dict_response)
+
+@app.route('/' + sensorID + '/parameters', method='GET')
+def get_sensor_details():
+    """ Return an overview of the different parameters and settings for the specified virtual sensor """
     if sensor is None:
-        return "NOK\n"
+        response.set_header('Content-Type', 'application/json; charset=UTF-8')
+        dict_response = dict()
+        dict_response['status'] = "NOK"
+        dict_response['error_message'] = "Error: sensor {} is not initialized yet...".format(sensorID)
+        return json.dumps(dict_response)
     else:
         return sensor.__repr__()
 
-@app.route('/' + sensorID + '/get/<parameter>', method='GET')
-def getSensorDetail(parameter):
+@app.route('/' + sensorID + '/parameters/<parameter>', method='GET')
+def get_sensor_capability(parameter):
+    """ Method to get a sensor parameter (capability) """
+    response.set_header('Content-Type', 'application/json; charset=UTF-8')
+    dict_response = dict()
     if sensor is None:
-        return "Error: sensor not initialized yet...\n"
-    elif parameter == 'battery':
-        return str(sensor.battery_level)
+        dict_response['status'] = "NOK"
+        dict_response['error_message'] = "Error: sensor {} is not initialized yet...".format(sensorID)
+    elif parameter in sensor.capabilities.keys():
+        dict_response['status'] = "OK"
+        dict_response['error_message'] = ""
+        dict_response['parameter'] = parameter
+        dict_response['value'] = sensor.capabilities[parameter]
     else:
-        return sensor.__repr__()
+        dict_response['status'] = "NOK"
+        dict_response['error_message'] = "Unknown parameter '{}' for sensor {}".format(parameter, sensorID)
+    return json.dumps(dict_response)
 
-sensorEndpoint = 'http://' + bottle_host + ':' + str(bottle_port) + '/' + sensorID
-sensor = VirtualSensor(sensorID, True, sensorEndpoint,
-                       [{'type': 'temperature', 'min_value': -100, 'max_value': +100, 'resolution': 0.5, 'format': float}])
+@app.route('/' + sensorID + '/parameters/<parameter>', method='POST')
+def set_sensor_capability(parameter):
+    """ Method to modify a sensor parameter (capability) """
+    response.set_header('Content-Type', 'application/json; charset=UTF-8')
+    dict_response = dict()
+    if parameter in sensor.capabilities.keys():
+        request_json_body = json.loads(request.body.read().decode('UTF-8'))
+        old_value = sensor.get_parameter(parameter)
+        new_value = request_json_body['new_value']
+        sensor.set_parameter(parameter, new_value)
+        dict_response['status'] = "OK"
+        dict_response['error_message'] = ""
+        dict_response['parameter'] = parameter
+        dict_response['old_value'] = old_value
+        dict_response['new_value'] = new_value
+    else:
+        dict_response['status'] = "NOK"
+        dict_response['error_message'] = "Unknown parameter '{}' for sensor {}".format(parameter, sensorID)
+    return json.dumps(dict_response)
+
+with open('../etc/capabilities.json') as capabilities_file:
+    capabilities = json.load(capabilities_file)
+
+# Virtual sensor creation and start of the sensor's main thread
+
+sensor = VirtualSensor(sensorID, True, sensorEndpoint, capabilities)
+
+# Start of a bottle server to handle calls to the sensor API
 
 run(app, host=bottle_host, port=bottle_port, debug=True)
--- _virtual_sensors/src/virtual_sensor.py
+++ _virtual_sensors/src/virtual_sensor.py
@@ -1,4 +1,10 @@
-class VirtualSensor(object):
+import logging
+import threading
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+class VirtualSensor(threading.Thread):
 	"""
 		Class for representing a virtual sensor, i.e. a sensor which produce observations from an optional input file.
 		Output format for observations: JSON
@@ -6,18 +12,82 @@ class VirtualSensor(object):
 	"""
 
 	def __init__(self, sensorID, enabled, endpoint, capabilities):
+		threading.Thread.__init__(self)
+		self.setDaemon(True)
+		self._stopevent = threading.Event() # to stop the main thread
 		self.sensorID = sensorID
 		self.enabled = enabled
 		self.endpoint = endpoint
-		self.capabilities = capabilities # list of capabilities e.g.: [{'type': 'temperature', 'min_value': -100, 'max_value': +100, 'resolution': 0.5, 'format': float}]
-		self.battery_level = 100 # in percentage
+		self.sensing = False
+
+		self.capabilities = capabilities # dict of capabilities e.g.: {'type': 'temperature', 'min_value': -100, 'max_value': 100, 'resolution': 0.5, 'format': float}
+		self.battery_level = self.capabilities['battery_level'] # in percentage
+		self.obs_consumption = self.capabilities['obs_consumption'] # how much battery is used when sensing one observation
+		self.infinite_battery = self.capabilities['infinite_battery'] # if set to True, all battery considerations are ignored
+
+		self.start() # start sensor's main thread
 
 	def __repr__(self):
 		return "Sensor ID: {}\n" \
                "Enabled: {}\n" \
+		       "Currently sensing observations: {}\n" \
                "Endpoint: {}\n" \
                "Capabilities: {}\n" \
-               "Battery level: {}%\n".format(self.sensorID, self.enabled, self.endpoint, self.capabilities, self.battery_level)
+               "Battery level: {}%\n".format(self.sensorID, self.enabled, self.sensing, self.endpoint, self.capabilities, self.battery_level)
 
+	def __del__(self):
+		self._stopevent.set()
 
+	def run(self):
+		""" Sensor's main thread. We should never stop this thread, except when destroying the sensor object """
+		while not self._stopevent.isSet():
+			if self.sensing:
+				logger.error("In sensor {} thread (freq={}s)".format(self.sensorID, self.capabilities['frequency']))
+
+				# TODO measure and send data
+				if not self.infinite_battery:
+					self.battery_level -= self.obs_consumption
+
+			self._stopevent.wait(self.capabilities['frequency']) # We pause based on sensor's frequency
+
+	# The following methods represent the API of the virtual sensor
+	# Sensor state (connection and observations measurement)
+
+	def enable_sensor(self, new_value):
+		""" Method to activate/deactivate a sensor, i.e. connect or disconnect it to the network """
+		if new_value:
+			if self.infinite_battery or self.battery_level > 0.0:
+				self.enabled = True
+		else:
+			self.enabled = False
+			self.enable_sensing_process(False)
+
+	def enable_sensing_process(self, new_value):
+		""" Method to start the observation acquisition process """
+		if new_value:
+			if 'frequency' in self.capabilities.keys() and self.capabilities['frequency'] > 0.0:
+				if self.enabled and not self.sensing:
+					self.sensing = True
+				else:
+					logger.error("Unable to start the observation acquisition process for sensor {}. The sensor is disabled.".format(self.sensorID))
+			else:
+				logger.error("Unable to retrieve 'frequency' capability for sensor {}. The acquisition process has not been started.".format(self.sensorID))
+		else:
+			self.sensing = False
+
+	# Sensor capabilities
+
+	def get_parameter(self, param_name):
+		return self.capabilities[param_name]
+
+	def set_parameter(self, param_name, new_value):
+		self.capabilities[param_name] = new_value
+
+	def recharge_battery(self):
+		self.battery_level = 100.0
+
+	# Events to randomly affect sensor or sensor measurement process
+	# Useful to introduce biased data or simulate sensor failures
+
+	# TODO
 
