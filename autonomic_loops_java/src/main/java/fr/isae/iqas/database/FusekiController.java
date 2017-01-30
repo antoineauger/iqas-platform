b071cc380c4661f904e10dcd150566d493e610b9
Antoine Auger <antoine.auger@isae.fr>
2017-01-30 17:51:21 +0100

    JENA test java: -Ontology final for starting evaluations -Basic pipelines: PipelineEco, PipelineFast and PipelineAccurate with only 1 QoO mechanism -More complex Pipeline with more mechanisms: inference issues, less precise

---
+++ autonomic_loops_java/src/main/java/fr/isae/iqas/database/FusekiController.java
@@ -0,0 +1,7 @@
+package fr.isae.iqas.database;
+
+/**
+ * Created by an.auger on 25/01/2017.
+ */
+public class FusekiController {
+}
--- jena_test_java/src/main/java/fr/isae/iqas/MainClass.java
+++ jena_test_java/src/main/java/fr/isae/iqas/MainClass.java
@@ -1,10 +1,22 @@
 package fr.isae.iqas;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.github.jsonldjava.core.JsonLdError;
+import com.github.jsonldjava.core.JsonLdOptions;
+import com.github.jsonldjava.core.JsonLdProcessor;
+import com.github.jsonldjava.utils.JsonUtils;
 import org.apache.jena.ontology.*;
+import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
+import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 import org.apache.jena.util.PrintUtil;
+import org.apache.jena.util.iterator.ExtendedIterator;
 
+import java.io.*;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import static org.apache.jena.ontology.OntModelSpec.OWL_MEM_MICRO_RULE_INF;
 
@@ -19,6 +31,82 @@ public class MainClass {
         }
     }
 
+    public static void execSelectAndProcess(String serviceURI, String query) throws JsonLdError, IOException {
+        QueryExecution q = QueryExecutionFactory.sparqlService(serviceURI,
+                query);
+
+        ResultSet m = q.execSelect();
+        //printStatements(m, null, null, null);
+
+        //For DESCRIBE
+//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//        m.write(outputStream, "JSON-LD");
+//        Map context = new HashMap();
+//        JsonLdOptions options = new JsonLdOptions();
+//        Object compact = JsonLdProcessor.expand(JsonUtils.fromString(new String(outputStream.toByteArray())), options);
+//        System.out.println(JsonUtils.toPrettyString(compact));
+//
+//        m.write(System.out, "JSON-LD");
+
+        //Model m = q.execConstruct();
+        /*QueryEngineHTTP qeHttp = (QueryEngineHTTP) q;
+        qeHttp.setModelContentType("application/rdf+xml");
+// Serialize it
+        Model m = q.execConstruct();
+        m.write(System.out, "TTL");
+        m.write(System.out, "JSON-LD");*/
+
+        //ResultSet results = null;
+        try {
+            //ResultSet results = q.execSelect();
+            //results = q.execSelect();
+
+            ResultSetFormatter.outputAsJSON(System.out, m);
+        }
+        finally {
+            q.close();
+        }
+
+
+
+
+        // write to a ByteArrayOutputStream
+        /*ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        ResultSetFormatter.outputAsJSON(outputStream, results);
+
+        // and turn that into a String
+        String json = new String(outputStream.toByteArray());
+
+        JSONObject t = new JSONObject("{ \"type\": \"uri\" , \"value\": \"http://isae.fr/iqas/qoo-ontology#iQAS\" }");
+        JSONObject s = new JSONObject(t.get("results");
+
+
+// Read the file into an Object (The type of this object will be a List, Map, String, Boolean,
+// Number or null depending on the root object in the file).
+        Object jsonObject = JsonUtils.fromString(json);
+// Create a context JSON map containing prefixes and definitions
+        Map context = new HashMap();
+        context.put("qoo", "http://isae.fr/iqas/qoo-ontology#");
+// Customise context...
+// Create an instance of JsonLdOptions with the standard JSON-LD options
+        JsonLdOptions options = new JsonLdOptions();
+// Customise options...
+// Call whichever JSONLD function you want! (e.g. compact)
+        Object compact = JsonLdProcessor.compact(jsonObject, context, options);
+// Print out the result (or don't, it's your call!)
+        System.out.println(JsonUtils.toPrettyString(compact));
+
+        System.out.println(json);
+
+        /*while (results.hasNext()) {
+            QuerySolution soln = results.nextSolution();
+            // assumes that you have an "?x" in your query
+            RDFNode x = soln.get("subject");
+            System.out.println(x);
+        }*/
+    }
+
     public static void main(String[] args) throws Exception {
         String schemaLocation = "file:/Users/an.auger/Documents/GIT/iQAS_platform/jena_test_java/iQAS_ontology/qoo-ontology.rdf";
         String qooIRI = "http://isae.fr/iqas/qoo-ontology#";
@@ -26,66 +114,80 @@ public class MainClass {
         String iotliteIRI = "http://purl.oclc.org/NET/UNIS/fiware/iot-lite#";
         String geoIRI = "http://www.w3.org/2003/01/geo/wgs84_pos#";
 
-        OntDocumentManager mgr = new OntDocumentManager();
-        //mgr.setProcessImports(false);
-        OntModelSpec s = new OntModelSpec( OntModelSpec.OWL_MEM);
-        s.setDocumentManager(mgr);
-        OntModel ontModelBase = ModelFactory.createOntologyModel(s);
-        ontModelBase.read(schemaLocation, "RDF/XML");
-
-        Model otherModel = ontModelBase.read("file:/Users/an.auger/Documents/GIT/iQAS_platform/jena_test_java/iQAS_ontology/sensors.jsonld", "JSON-LD");
-
-        ontModelBase.add(otherModel);
-
-        // create the reasoning model using the base
-        OntModel inf = ModelFactory.createOntologyModel( OWL_MEM_MICRO_RULE_INF, ontModelBase );
-
-        //Properties
-        Property onPlatform = inf.getProperty( ssnIRI + "onPlatform" );
-        Property provides = inf.getProperty( qooIRI + "provides" );
-        Property supports = inf.getProperty( qooIRI + "supports" );
-        Property isSupportedBy = inf.getProperty( qooIRI + "isSupportedBy" );
+        execSelectAndProcess(
+                "http://localhost:3030/qoo-onto/sparql",
+                "PREFIX ssn: <http://purl.oclc.org/NET/ssnx/ssn#>\n" +
+                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                        "PREFIX qoo: <http://isae.fr/iqas/qoo-ontology#>\n" +
+                        "\n" +
+                        "SELECT ?subject\n" +
+                        "WHERE {\n" +
+                        "?subject rdf:type qoo:PipelineEco\n" +
+                        "}\n" +
+                        "LIMIT 25"
+                        );
 
 
-        Individual iqas = inf.getIndividual( qooIRI + "iQAS" );
-        Property attachedSystem = inf.getProperty(ssnIRI + "attachedSystem");
-        NodeIterator attachedToIQAS = iqas.listPropertyValues(attachedSystem);
-        for (NodeIterator i = attachedToIQAS; i.hasNext(); ) {
-            System.out.println( "Attached: " + i.next() );
-        }
-
-
-        //TODO replace
-        OntClass sensingDevice = inf.getOntClass( qooIRI + "visibility" );
-        Individual sensor01 = inf.getIndividual( qooIRI + "visibility" );
-
-
-        /*List<RDFNode> r = sensor01.listPropertyValues(hasMeasurementCapability).toList();
-        Individual o = inf.getIndividual(r.get(0).toString());
-
-        RDFNode q = o.getPropertyValue(hasMeasurementProperty);
-        Individual p = inf.getIndividual(q.toString());*/
-
-
-        // list the asserted types
-        for (Iterator<Resource> i = sensor01.listRDFTypes(false); i.hasNext(); ) {
-            System.out.println( iqas.getURI() + " is asserted in class " + i.next() );
-        }
-
-        // list the inferred types
-        for (Iterator<Resource> i = sensor01.listRDFTypes(false); i.hasNext(); ) {
-            System.out.println( iqas.getURI() + " is inferred to be in class " + i.next() );
-        }
-
-        // list the individuals
-        /*for (ExtendedIterator<? extends OntResource> i = sensor01.listInstances(); i.hasNext(); ) {
-            System.out.println( i.next() );
-        }*/
-
-        // list the properties
-        for (StmtIterator i = sensor01.listProperties(); i.hasNext(); ) {
-            System.out.println( i.next() );
-        }
+//        OntDocumentManager mgr = new OntDocumentManager();
+//        //mgr.setProcessImports(false);
+//        OntModelSpec s = new OntModelSpec( OntModelSpec.OWL_MEM);
+//        s.setDocumentManager(mgr);
+//        OntModel ontModelBase = ModelFactory.createOntologyModel(s);
+//        ontModelBase.read(schemaLocation, "RDF/XML");
+//
+//        Model otherModel = ontModelBase.read("file:/Users/an.auger/Documents/GIT/iQAS_platform/jena_test_java/iQAS_ontology/sensors.jsonld", "JSON-LD");
+//
+//        ontModelBase.union(otherModel);
+//
+//        // create the reasoning model using the base
+//        OntModel inf = ModelFactory.createOntologyModel( OWL_MEM_MICRO_RULE_INF, ontModelBase );
+//
+//        //Properties
+//        Property onPlatform = inf.getProperty( ssnIRI + "onPlatform" );
+//        Property provides = inf.getProperty( qooIRI + "provides" );
+//        Property supports = inf.getProperty( qooIRI + "supports" );
+//        Property isSupportedBy = inf.getProperty( qooIRI + "isSupportedBy" );
+//
+//
+//        Individual iqas = inf.getIndividual( qooIRI + "iQAS" );
+//        Property attachedSystem = inf.getProperty(ssnIRI + "attachedSystem");
+//        NodeIterator attachedToIQAS = iqas.listPropertyValues(attachedSystem);
+//        for (NodeIterator i = attachedToIQAS; i.hasNext(); ) {
+//            System.out.println( "Attached: " + i.next() );
+//        }
+//
+//
+//        //TODO replace
+//        OntClass sensingDevice = inf.getOntClass( qooIRI + "visibility" );
+//        Individual sensor01 = inf.getIndividual( qooIRI + "sensor01" );
+//
+//
+//        /*List<RDFNode> r = sensor01.listPropertyValues(hasMeasurementCapability).toList();
+//        Individual o = inf.getIndividual(r.get(0).toString());
+//
+//        RDFNode q = o.getPropertyValue(hasMeasurementProperty);
+//        Individual p = inf.getIndividual(q.toString());*/
+//
+//
+//        // list the asserted types
+//        for (Iterator<Resource> i = sensor01.listRDFTypes(false); i.hasNext(); ) {
+//            System.out.println( iqas.getURI() + " is asserted in class " + i.next() );
+//        }
+//
+//        // list the inferred types
+//        for (Iterator<Resource> i = sensor01.listRDFTypes(false); i.hasNext(); ) {
+//            System.out.println( iqas.getURI() + " is inferred to be in class " + i.next() );
+//        }
+//
+//        // list the individuals
+//        /*for (ExtendedIterator<? extends OntResource> i = sensor01.listInstances(); i.hasNext(); ) {
+//            System.out.println( i.next() );
+//        }*/
+//
+//        // list the properties
+//        for (StmtIterator i = sensor01.listProperties(); i.hasNext(); ) {
+//            System.out.println( i.next() );
+//        }
 
 
 
